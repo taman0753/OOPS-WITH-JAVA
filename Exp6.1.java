e37483e358e2d17910be882711bf10937b17d55a
taman0753 <taman0753@gmail.com>
2020-05-15 11:50:50 +0530

    exp6.1final

--- Exp6.1.java
+++ Exp6.1.java
@@ -14,22 +14,22 @@ System.out.println("ExtendsThread : Counter : " + counter);
 }
 public class Thread_exp1 {
 public static void main(String args[]) throws Exception {
-//Multiple threads share the same object.
+
 ImplementsRunnable rc = new ImplementsRunnable();
 Thread t1 = new Thread(rc);
 t1.start();
-Thread.sleep(1000); // Waiting for 1 second before starting next thread
+Thread.sleep(1000); 
 Thread t2 = new Thread(rc);
 t2.start();
-Thread.sleep(1000); // Waiting for 1 second before starting next thread
+Thread.sleep(1000); 
 Thread t3 = new Thread(rc);
 t3.start();
 ExtendsThread tc1 = new ExtendsThread();
 tc1.start();
-Thread.sleep(1000); // Waiting for 1 second before starting next thread
+Thread.sleep(1000); 
 ExtendsThread tc2 = new ExtendsThread();
 tc2.start();
-Thread.sleep(1000); // Waiting for 1 second before starting next thread
+Thread.sleep(1000); 
 ExtendsThread tc3 = new ExtendsThread();
 tc3.start();
 }
