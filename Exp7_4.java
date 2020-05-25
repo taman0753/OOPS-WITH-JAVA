fa021f6edb62d62171e969173691e9367de3ed70
taman0753 <taman0753@gmail.com>
2020-05-25 09:55:48 +0530

    exp7.4

---
+++ Exp7_4.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class collision extends Exception
+{
+collision(String s)
+{ super(s); }
+}
+class Exp7_4
+{
+public static void main(String ar[])
+{
+String car1=null,car2=null;
+try
+{
+Scanner sc= new Scanner(System.in);
+System.out.println("Enter direction of car1(left/right):");
+car1= sc.nextLine();
+System.out.println("Enter the direction of car2(left/right):"); 
+ car2= sc.nextLine();
+if(!car1.equals(car2))
+throw new collision("car2 has to go on "+ car1 +" direction");
+}
+catch(collision e)
+{
+System.out.println(e); 
+car2=car1;
+System.out.println("Changed the direction of car2");
+}
+}}
+
