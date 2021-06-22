982786e22685da9fbd07a3b34981a510bd7d8de6
AMAN TYAGI <500069023@stu.upes.ac.in>
2021-06-22 11:07:47 +0530

    Create BubbleSort_1.java

---
+++ BubbleSort_1.java
@@ -0,0 +1,30 @@
+public class Arrays {
+
+	public static void main(String[] args) {
+		int a[] = { 2,9,3,4,5 };
+		int n = a.length;
+		for ( int i =0 ; i<n-1 ; i++) {
+			for ( int j = 0 ; j<n-1 ; j++) {
+				 
+				if (a[j+1] <a[j]) {
+					
+					int temp = a[j];
+					a[j] = a[j+1];
+					a[j+1] = temp;
+					
+					
+				}
+					
+		}
+			
+		}
+	
+		for ( int item: a) {
+		System.out.print(item + "  " );
+		}
+		}
+		
+
+	}
+
+
